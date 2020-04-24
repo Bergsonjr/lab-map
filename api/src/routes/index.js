b104144f1e6443db055e550568641e2f66f8bc33
Bergsonjr <bergsonjr@icloud.com>
2020-04-24 09:20:22 -0300

    include path equipment in routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const equipment = require('./equipment');
 const register = require('./register');
 const category = require('./category');
 const session = require('./session');
 
+router.use('/equipment', equipment);
 router.use('/register', register);
 router.use('/category', category);
 router.use('/session', session);
